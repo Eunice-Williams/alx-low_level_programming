c29744b2c8fcf1399671f1798696c0af649b040f
Eunice-Williams <molake18williams@gmail.com>
2023-09-15 23:57:24 +0000

    Printing sizes of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints the variable sizes of computer data types
+ * main - A program that prints the various sizes of computer data types
  *
  * Return: 0 always (success)
  */
@@ -12,10 +12,10 @@ long int x;
 long long int y;
 float f;
 printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: 1 byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: 1 byte(s)\n", (unsigned long)sizeof(x));
-printf("Size of a long long int: 1 byte(s)\n", (unsigned long)sizeof(y));
-printf("Size of a float: 1 byte(s)\n", (unsigned long)sizeof(z));
+printf("Size of a int: 4 byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: 8 byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of a long long int: 8 byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a float: 4 byte(s)\n", (unsigned long)sizeof(z));
 return (0);
 }
 
