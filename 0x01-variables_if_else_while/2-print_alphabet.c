222b44e9a95a7990ec65a267266f3ea9a3f415b8
Eunice-Williams <molake18williams@gmail.com>
2023-09-16 02:55:45 +0000

    Print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets in lowercase
+ *
+ * Return: 0 always (success)
+ */
+intmain(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z' ; i++)
+		printf(i);
+	return (0);
+}
