3ecfa40fd9d79f0cc0b385614157dece3972322d
Eunice-Williams <molake18williams@gmail.com>
2023-09-16 14:37:33 +0000

    All possible combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible combinations of two digits
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int x = '0';
+	int y = '0';
+
+	while (x <= '9')
+	{
+		while (y <= '9')
+		{
+			if (!(x > y) || (x == y))
+			{
+				putchar(x);
+				putchar(y);
+				if (x == '8' && y == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++y;
+		}
+		y = '0';
+		++x;
+	}
+	return (0);
+}
+
