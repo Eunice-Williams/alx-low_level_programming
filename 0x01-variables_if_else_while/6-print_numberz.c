7399ada7b6d73bd517f451620958cbe8f8af32ba
Eunice-Williams <molake18williams@gmail.com>
2023-09-16 12:55:25 +0000

    Printing numbers in base 10 using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers in base 10 using putchar()
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int digits;
+
+	while (digit < 10)
+	{
+		putchar(digits);
+		digit++;
+	}
+	putchar("\n");
+	return (0);
+}
+
