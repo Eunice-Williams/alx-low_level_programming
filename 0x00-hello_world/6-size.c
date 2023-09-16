5aa21afc4dd1fbe6354bcf065e40c1d18822d19c
Eunice-Williams <molake18williams@gmail.com>
2023-09-16 00:24:34 +0000

    Printing sizes of variuos types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int b;
 long int x;
 long long int y;
 float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %d byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %f byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
