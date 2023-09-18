8dcf55f58b22bd5ffc0f6a4fe00e74f9386331b1
Eunice-Williams <molake18williams@gmail.com>
2023-09-18 19:01:42 +0000

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - A function that prints a string
+ * @str: - pointer to the string
+ * Return: string and a newline
+ */
+void _puts(char *str)
+{
+	int x = 0;
+
+	while (str[x])
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
